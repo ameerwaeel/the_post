c37c541551c84ae9e1561271266f21fc7af844ca
ameerwaeel <ameer.waeel99@email.com>
2025-07-27 13:07:16 +0300

    admin_panal

--- contact_us/admin.py
+++ contact_us/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from.models import *
+admin.site.register(Interests)
+admin.site.register(ContactUS)
+admin.site.register(ChatContactUS)
+admin.site.register(NewsLetter)
--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+admin.site.register(OurWorks)
+admin.site.register(WhoWeAre)
+admin.site.register(Logo)
+admin.site.register(Projects)
+admin.site.register(OurSolution)
+admin.site.register(OurResults)
+admin.site.register(Imgs)
+admin.site.register(HomepageProjects)
+admin.site.register(Counters)
+admin.site.register(Services)
